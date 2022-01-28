dae6b2848f484a995012ce241a5e550f0cd34384
Ahmed Ali <ahmdaliielsayed@gmail.com>
2022-01-28 20:53:47 +0200

    applying MVP

---
+++ Sports/Model/Services/APICall.swift
@@ -0,0 +1,10 @@
+//
+//  APICall.swift
+//  Sports
+//
+//  Created by Ahmed on 1/28/22.
+//  Copyright © 2022 Ahmed Ali. All rights reserved.
+//
+
+import Foundation
+
---
+++ Sports/Presenter/HomePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  HomePresenter.swift
+//  Sports
+//
+//  Created by Ahmed on 1/28/22.
+//  Copyright © 2022 Ahmed Ali. All rights reserved.
+//
+
+import Foundation
+
+class HomePresenter {
+    
+}
